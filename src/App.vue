bb69cd1b11d3b35c93f3246194365b2414ecca18
YgorLourenco <44757992+YgorLourenco@users.noreply.github.com>
2022-05-31 22:35:15 -0300

    Atualizacao

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row mt-5" v-if="listaPositivo.length > 0">
+      <div class="col">
+        <h2>Positivo</h2>
+        <line-chart :chartData="listaPositivo" :options="chartOptions" label="Positive"></line-chart>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Importando o axios para conectar com a API
+import axios from 'axios';
+
+// Biblioteca moment formata dados
+import moment from 'moment';
+
+// Importando o componente de grafico
+import LineChart from './components/LineChart.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // eslint-disable-next-line vue/no-unused-components
+    LineChart
+  },
+  // Criando os arrays que vão armazenar as informações da api
+  data(){
+    return{
+    listaPositvo: [],
+    listaHospitalizado: [],
+    listaEmUTI: [],
+    listaVentiladores: [],
+    listaRecuperados: [],
+    listaMortes: [],
+    chartOptions: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+    }
+  },
+  // Armazenando a requisição na variavel "data"
+  async created() {
+    const {data} = await axios.get("https://api.covidtracking.com/v1/us/daily.json")
+    //console.log(data)
+
+    // Formatando os dados de tipo data
+    data.forEach(d => {
+      const date = moment(d.date, "YYYYMMDD").format("MM/DD")
+
+      // Armazenando cada tipo de dado da API
+      const {
+        positive,
+        hospitalizedCurrently,
+        inIcuCurrently,
+        onVentilatorCurrently,
+        recovered,
+        death
+      } = d;
+
+      // Armazenando os objeto dos dados em arrays
+      this.listaPositvo.push({date, total: positive})
+      this.listaHospitalizado.push({date, total: hospitalizedCurrently})
+      this.listaEmUTI.push({date, total: inIcuCurrently})
+      this.listaVentiladores.push({date, total: onVentilatorCurrently})
+      this.listaRecuperados.push({date, total: recovered})
+      this.listaMortes.push({date, total: death})
+
+      console.log(this.listaPositvo)
+    });
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,33 @@
+<script>
+// Importar grafico de linha para o componente
+import {Line} from "vue-chartjs"
+
+export default {
+    // Configurando o grafico de linha
+    extends: Line,
+    props:{
+        label:{
+            type: String
+        },
+        chartData:{
+            type: Array
+        },
+        options: {
+            type: Object
+        }
+    },
+    // Montando os dados no grafico
+    mounted(){
+        const dates = this.chartData.map(d => d.date).reverse()
+        const totals = this.chartData.map(d => d.total).reverse()
+
+        this.renderChart({
+            labels:dates,
+            datasets: ({
+                label: this.label,
+                data: totals
+            })
+        }, this.options)
+    }
+}
+</script>
